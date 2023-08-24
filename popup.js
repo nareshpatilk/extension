82550cafff6b2a9844c767ab08ec1503e954c2a6
Keerthan HS <68416693+Keerthan-HS@users.noreply.github.com>
2023-08-25 02:11:48 +0530

    Read Ag grid table data

--- popup.js
+++ popup.js
@@ -1,19 +1,33 @@
 
+const onDOM = () => {
+
+  let abc = {}
+  //Process the datatype and send it extension, need to find a proper way to create a data structure
+  Array.from(document.getElementsByClassName("ag-cell-value")).forEach(x => {
+    abc[x.getAttribute('col-id')] = Array.isArray(abc[x.getAttribute('col-id')]) ? [...abc[x.getAttribute('col-id')],x.innerHTML]:[x.innerHTML]
+  })
+
+  return abc
+}
+
+
 async function onWindowLoad() {
-    const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   let result;
   try {
-    [{result}] = await chrome.scripting.executeScript({
-      target: {tabId: tab.id},
-      func: () => document.documentElement.outerHTML,
+    [{ result }] = await chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: onDOM,
     });
+
   } catch (e) {
     document.body.textContent = 'Cannot access page';
     return;
   }
-  // process the result
-  document.body.textContent = result;
-    
+
+
+  console.log(result,'extension received')
+
 }
 
 document.getElementById('scrapeEmails').addEventListener("click", onWindowLoad);
