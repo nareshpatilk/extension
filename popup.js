75382603b3f428e85aa72716f0fb21bd11e6ea78
nareshpatilk <40860500+nareshpatilk@users.noreply.github.com>
2023-08-25 15:00:47 +0530

    exteract data for normal tables

--- popup.js
+++ popup.js
@@ -1,19 +1,89 @@
-
-async function onWindowLoad() {
-    const [tab] = await chrome.tabs.query({active: true, currentWindow: true});
+async function scrapeHtmlCode() {
+  const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
   let result;
   try {
-    [{result}] = await chrome.scripting.executeScript({
-      target: {tabId: tab.id},
-      func: () => document.documentElement.outerHTML,
+    [{ result }] = await chrome.scripting.executeScript({
+      target: { tabId: tab.id },
+      func: fetchTableHeader,
     });
   } catch (e) {
-    document.body.textContent = 'Cannot access page';
+    document.body.textContent = "Cannot access page";
     return;
   }
   // process the result
+  console.log(result);
   document.body.textContent = result;
-    
 }
 
-document.getElementById('scrapeEmails').addEventListener("click", onWindowLoad);
+async function fetchTableHeader() {
+  const data = document.documentElement.innerHTML;
+  const headers = [];
+  try {
+    const table = document.querySelector("table");
+    const thead = table.querySelectorAll("thead");
+    const th = table.querySelectorAll("th");
+    let i = 0;
+    for (const header of th) {
+      let val = header.innerText;
+      if (val.toString().length > 0) {
+        headers.push(val.trim());
+      } else {
+        headers.push(++i);
+      }
+    }
+
+    const tbody = table.querySelector("tbody");
+    const trow = tbody.querySelectorAll("tr");
+
+    const tabledata = [];
+    for (const row of trow) {
+      const tds = row.querySelectorAll("td");
+      const eachRow = [];
+      for (const td of tds) {
+        let val = td.innerText;
+        if (val.toString().length > 0) {
+          eachRow.push(val.trim());
+        } else {
+          eachRow.push("scrpaeeeeeeeeeeee");
+        }
+      }
+      tabledata.push(eachRow);
+    }
+
+    const manipulateData = new Map();
+    let map1 = new Map();
+    
+    for(let header of headers){
+      manipulateData.set(header, []);
+    }
+
+    for(let l = 0; l < tabledata.length; l++){
+      for( k = 0; k < tabledata[l].length; k++){
+        let arr = manipulateData.get(headers[k]);        
+        arr.push(tabledata[l][k]);
+        manipulateData.set(headers[k], arr)
+      }
+    }
+
+    let finalResult = new Map();
+
+    manipulateData.forEach((v, k) => {
+      
+      let res = v.reduce((occurrences, item) => {
+        occurrences[item] = (occurrences[item] || 0) + 1;
+        return occurrences;
+      }, [])
+      finalResult.set(k, res);
+    })
+
+    console.log('<<<<', finalResult);
+  } catch (err) {
+    alert(err);
+    console.log(err);
+  }
+  return headers;
+}
+
+document
+  .getElementById("scrapeEmails")
+  .addEventListener("click", scrapeHtmlCode);
