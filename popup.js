f4f7c7db9ba0642476e71f99c89de8427e9ff47c
nareshpatilk <40860500+nareshpatilk@users.noreply.github.com>
2023-08-25 15:38:35 +0530

    modified

--- popup.js
+++ popup.js
@@ -4,19 +4,74 @@ async function scrapeHtmlCode() {
   try {
     [{ result }] = await chrome.scripting.executeScript({
       target: { tabId: tab.id },
-      func: fetchTableHeader,
+      func: typeOfTable,
     });
   } catch (e) {
     document.body.textContent = "Cannot access page";
     return;
   }
+
+console.log('herere is the data = > ' + result)
   // process the result
-  console.log(result);
-  document.body.textContent = result;
+  Highcharts.chart("container", {
+    chart: {
+      plotBackgroundColor: null,
+
+      plotBorderWidth: null,
+
+      plotShadow: false,
+
+      type: "pie",
+    },
+
+    title: {
+      text: "Dynamic Dashboards",
+
+      align: "left",
+    },
+
+    tooltip: {
+      pointFormat: "{series.name}: <b>{point.percentage:.1f}%</b>",
+    },
+
+    accessibility: {
+      point: {
+        valueSuffix: "%",
+      },
+    },
+
+    plotOptions: {
+      pie: {
+        allowPointSelect: true,
+
+        cursor: "pointer",
+
+        dataLabels: {
+          enabled: true,
+
+          format: "<b>{point.name}</b>: {point.percentage:.1f} %",
+        },
+      },
+    },
+
+    series: [
+      {
+        name: "Brands",
+
+        colorByPoint: true,
+
+        data: result,
+      },
+    ],
+  });
 }
 
-async function fetchTableHeader() {
+function normalTableExtract() {}
+
+async function typeOfTable() {
   const data = document.documentElement.innerHTML;
+  let finalResult = new Map();
+  let chartResult = [];
   const headers = [];
   try {
     const table = document.querySelector("table");
@@ -52,38 +107,41 @@ async function fetchTableHeader() {
 
     const manipulateData = new Map();
     let map1 = new Map();
-    
-    for(let header of headers){
+
+    for (let header of headers) {
       manipulateData.set(header, []);
     }
 
-    for(let l = 0; l < tabledata.length; l++){
-      for( k = 0; k < tabledata[l].length; k++){
-        let arr = manipulateData.get(headers[k]);        
+    for (let l = 0; l < tabledata.length; l++) {
+      for (k = 0; k < tabledata[l].length; k++) {
+        let arr = manipulateData.get(headers[k]);
         arr.push(tabledata[l][k]);
-        manipulateData.set(headers[k], arr)
+        manipulateData.set(headers[k], arr);
       }
     }
 
-    let finalResult = new Map();
+    
 
     manipulateData.forEach((v, k) => {
-      
       let res = v.reduce((occurrences, item) => {
         occurrences[item] = (occurrences[item] || 0) + 1;
         return occurrences;
-      }, [])
+      }, []);
       finalResult.set(k, res);
-    })
+    });
 
-    console.log('<<<<', finalResult);
+    const reduced = finalResult.get('Website');
+    const result = Object.keys(reduced).map((item) => {
+      return { name: item, y: reduced[item] };
+    });
+    //console.log(result);
+
+    chartResult = result;
   } catch (err) {
     alert(err);
     console.log(err);
   }
-  return headers;
+  return chartResult;
 }
 
-document
-  .getElementById("scrapeEmails")
-  .addEventListener("click", scrapeHtmlCode);
+document.getElementById("scrapeData").addEventListener("click", scrapeHtmlCode);
